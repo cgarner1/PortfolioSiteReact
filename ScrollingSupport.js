b7852a60c0b7a9e329d23616b6ec2989f97239f6
Chris <seratodownloads@gmail.com>
2020-06-26 23:45:48 -0400

    Updates

---
+++ ScrollingSupport.js
@@ -0,0 +1,3 @@
+const scrollSWE = new SmoothScroll('.triple-row-grid a[href*="#/#SWE"]', { speed: 300 });
+const scrollML = new SmoothScroll('.triple-row-grid Link[href*="ML"]', { speed: 300 });
+const scrollGD = new SmoothScroll('.triple-row-grid Link[href*="GD"]', { speed: 300 });
---
+++ precache-manifest.85dd98b85710c73973ea10ffe371f1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9121bda364a9e623c25d9bc70f4ae7c",
+    "url": "/PortfolioSiteReact/index.html"
+  },
+  {
+    "revision": "6ffa006d46ab512c15a8",
+    "url": "/PortfolioSiteReact/static/css/main.175b36c1.chunk.css"
+  },
+  {
+    "revision": "624d32c9262c57fec947",
+    "url": "/PortfolioSiteReact/static/js/2.5dfb6f54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PortfolioSiteReact/static/js/2.5dfb6f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffa006d46ab512c15a8",
+    "url": "/PortfolioSiteReact/static/js/main.af2f3e3e.chunk.js"
+  },
+  {
+    "revision": "8ee502c7256371fdbf42",
+    "url": "/PortfolioSiteReact/static/js/runtime-main.c6cbfd77.js"
+  }
+]);
