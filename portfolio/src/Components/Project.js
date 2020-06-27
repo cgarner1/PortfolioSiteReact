b61ffc1d93486a83f40ddfe467cd039a613f29a8
Chris <seratodownloads@gmail.com>
2020-06-26 23:40:19 -0400

    fixed bug where images would not show

--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -6,7 +6,7 @@ export function Project(props) {
         <div className="projects-container">
             <div className="project-info-container">
                 <div className="project-content">
-                    <div className="project-img-container"><img className="proj-img" src={props.img}></img></div>
+                    <div className="project-img-container"><img className="proj-img" src={process.env.PUBLIC_URL + props.img}></img></div>
                     <div className="proj-text-container">
                         <h3>{props.name}</h3>
                         <h3>{props.date}</h3>
--- portfolio/src/Components/ProjectsPage.js
+++ portfolio/src/Components/ProjectsPage.js
@@ -30,36 +30,36 @@ export function ProjectsPage() {
 
                     {/* SWE */}
                     <section id="SWE"><h2>Software Engineering</h2></section>
-                    <Project name="ML Models to XLSX" date="Professional - 2019" img="./images/xlsx.png">
+                    <Project name="ML Models to XLSX" date="Professional - 2019" img="/images/xlsx.png">
                         Built and tested an internal API used to automatically extract performance data from trained machine learning models,
                         and organize that data elegantly in xlsx files, and JSON files to be consumed by other services.
                   </Project>
 
-                    <Project name="Full-Stack .Net Core Demo" date="Personal - 2020" img="./images/ASPDemo.png">
+                    <Project name="Full-Stack .Net Core Demo" date="Personal - 2020" img="/images/ASPDemo.png">
                         Learned the ins and outs of full-stack developmnent using .Net Core MVC, Entity Framework, and Bootstrap to build and deploy a web application with data persistence, user authentication,
                         and asynchronous API calls using RESTful conventions.
                   </Project>
 
-                    <Project name="Headphone Finder Site" date="Personal - 2020" img="./images/Site_Showoff.gif">
+                    <Project name="Headphone Finder Site" date="Personal - 2020" img="/images/Site_Showoff.gif">
                         Designed and implemented a dynamic front-end for a website designed to find optimal prices on headphones. The user expirience was designed to be easy to navigate,
                         yet visibly pleasing.
                   </Project>
 
                     {/* ML */}
                     <section id="ML"><h2>Machine Learning</h2></section>
-                    <Project name="Deep Learning Environment" date="Personal - 2019" img="./images/deep-q.gif">
+                    <Project name="Deep Learning Environment" date="Personal - 2019" img="/images/deep-q.gif">
                         Developed an AI environment from scratch designed to train reinforcement learning algorithms (Neural Networks, Genetic Algorithms, etc). The environment was based on
                       <a href=""> OpenAI's Gym API.</a>  Designed a custom Deep-Q algorithm that then learned and mastered the environment.
                   </Project>
 
-                    <Project name="Neural Network CLI App" date="Personal - 2019" img="./images/cmd_app_code.png">
+                    <Project name="Neural Network CLI App" date="Personal - 2019" img="/images/cmd_app_code.png">
                         Developed easy to use command line application designed to quickly choose, train, and implement multiple Convolutional Neural Networks using PyTorch and Python's
                         CLI tools.
                   </Project>
 
                     {/* Game Dev */}
                     <section id="GD"><h2>Game Deveopment</h2></section>
-                    <Project name="Traingle Rush" date="Personal - 2020" img="./images/trirush.png">
+                    <Project name="Traingle Rush" date="Personal - 2020" img="/images/trirush.png">
                         Collaborated with Elon University students to build a 2D shoot-em-up in Unity. Triangle Rush featured physics-based movement, the ability to grab and shoot back items
                         in the game world, mutliple enemy types and power-ups.
                   </Project>
